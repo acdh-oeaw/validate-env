0ffd4759c4f7dfbfc42ab92a25e2d29a23b8466c
Stefan Probst <stefanprobst@posteo.de>
2025-12-03 17:41:00 +0100

    feat: add entrypoint for runtime env vars

---
+++ .changeset/major-peas-prove.md
@@ -0,0 +1,5 @@
+---
+"@acdh-oeaw/validate-env": minor
+---
+
+add entrypoint for runtime env vars
---
+++ src/runtime.ts
@@ -0,0 +1,37 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { err, ok, type Result } from "@acdh-oeaw/lib";
+
+import { ValidationError } from "./index.js";
+
+type Input = Record<string, unknown>;
+
+interface CreateEnvParams<TPrivate extends Input> {
+	schema: (environment: Input) => Result<TPrivate, ValidationError>;
+	environment: Record<keyof NoInfer<TPrivate>, unknown>;
+	validation?: "disabled" | "enabled";
+}
+
+export function createEnv<TPrivate extends Input>({
+	schema,
+	environment,
+	validation,
+}: CreateEnvParams<TPrivate>): Result<Readonly<TPrivate>, ValidationError> {
+	for (const [key, value] of Object.entries(environment)) {
+		if (value === "") {
+			environment[key as keyof typeof environment] = undefined;
+		}
+	}
+
+	if (validation === "disabled") {
+		return ok(Object.freeze(environment as any));
+	}
+
+	const runtime = schema(environment);
+
+	if (ValidationError.is(runtime.error)) {
+		return err(runtime.error);
+	}
+
+	return ok(Object.freeze(runtime.value as any));
+}
