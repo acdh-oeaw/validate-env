31db0dca7c69ce3e2a7f2bfc5999cf6b817bb313
stefanprobst <stefanprobst@posteo.de>
2025-12-03 17:43:28 +0100

    feat: add entrypoint for runtime env vars (#15)
    this adds a separate entrypoint for validating runtime env vars. this is
    e.g. useful in a node.js environment. it takes care of things like
    treating empty strings as undefined which helps when `.env` files have
    an uncommented `VAR=`.

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "tsdown";
 export default defineConfig({
 	clean: true,
 	dts: true,
-	entry: ["./src/index.ts", "./src/astro.ts", "./src/next.ts"],
+	entry: ["./src/index.ts", "./src/astro.ts", "./src/next.ts", "./src/runtime.ts"],
 	format: ["esm"],
 	minify: false,
 	sourcemap: true,
